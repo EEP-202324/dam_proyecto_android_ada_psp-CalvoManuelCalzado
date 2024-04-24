ac20f3188a9b92c5a4ab0863455e8c75870fab3c
CalvoManuelCalzado <Manuel-Calvo1@eep-igroup.com>
2024-04-24 23:17:04 +0200

    Creación proyecto Spring

---
+++ Spring/pakamaka/src/test/java/example/pakamaka/PakamakaApplicationTests.java
@@ -0,0 +1,13 @@
+package example.pakamaka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PakamakaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
