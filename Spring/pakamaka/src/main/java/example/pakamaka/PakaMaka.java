6d2f98e885d5c90424bd4c68d8d3ca96b68e7943
CalvoManuelCalzado <Manuel-Calvo1@eep-igroup.com>
2024-04-28 14:41:12 +0200

    implementando el Get

--- Spring/pakamaka/src/main/java/example/pakamaka/PakaMaka.java
+++ Spring/pakamaka/src/main/java/example/pakamaka/PakaMaka.java
@@ -1,5 +1,8 @@
 package example.pakamaka;
 
+import org.springframework.data.annotation.Id;
 
-record CashCard(Long id, String nombre, String apellido, String email, Double numero) {
+record PakaMaka (@Id Long id, String name, String surname, String email, int number, String Country) {
+
 }
+             
