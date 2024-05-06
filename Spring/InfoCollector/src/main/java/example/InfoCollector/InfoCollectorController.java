71a73c31dd54e20644205ae98e0966483d1459ea
CalvoManuelCalzado <Manuel-Calvo1@eep-igroup.com>
2024-05-06 20:24:15 +0200

    m2

--- Spring/InfoCollector/src/main/java/example/InfoCollector/InfoCollectorController.java
+++ Spring/InfoCollector/src/main/java/example/InfoCollector/InfoCollectorController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @RestController
 @RequestMapping("/infocollectors/")
@@ -26,5 +27,5 @@ class InfoCollectorController {
 	        return ResponseEntity.notFound().build();
 	    }
 	}
-
+	
 }
--- Spring/InfoCollector/src/test/java/example/InfoCollector/InfoCollectorApplicationTests.java
+++ Spring/InfoCollector/src/test/java/example/InfoCollector/InfoCollectorApplicationTests.java
@@ -1,6 +1,5 @@
 package example.InfoCollector;
 
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,40 +14,41 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class InfoCollectorApplicationTests {
-    @Autowired
-    TestRestTemplate restTemplate;
+	@Autowired
+	TestRestTemplate restTemplate;
 
-    @Test
-    void shouldReturnAInfoCollectorWhenDataIsSaved() {
-        ResponseEntity<String> response = restTemplate.getForEntity("/infocollectors/99", String.class);
+	@Test
+	void shouldReturnAInfoCollectorWhenDataIsSaved() {
+		ResponseEntity<String> response = restTemplate.getForEntity("/infocollectors/99", String.class);
 
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
 
-        DocumentContext documentContext = JsonPath.parse(response.getBody());
-        Number id = documentContext.read("$.id");
-        assertThat(id).isEqualTo(99);
-        
-        String name = documentContext.read("$.name");
-        assertThat(name).isEqualTo("Jose");
-        
-        String surname = documentContext.read("$.surname");
-        assertThat(surname).isEqualTo("Garcia");
-        
-        String email = documentContext.read("$.email");
-        assertThat(email).isEqualTo("josegarcia@gmail.com");
-        
-        int number = documentContext.read("$.number");
-        assertThat(number).isEqualTo(640882919);
-        
-        String country = documentContext.read("$.country");
-        assertThat(country).isEqualTo("España");
-    }
-    
-    @Test
-    void shouldNotReturnAInfoControllerWithAnUnknownId() {
-      ResponseEntity<String> response = restTemplate.getForEntity("/infocontrollers/1000", String.class);
+		DocumentContext documentContext = JsonPath.parse(response.getBody());
+		Number id = documentContext.read("$.id");
+		assertThat(id).isEqualTo(99);
 
-      assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
-      assertThat(response.getBody()).isNotNull();
-    }
+		String name = documentContext.read("$.name");
+		assertThat(name).isEqualTo("Jose");
+
+		String surname = documentContext.read("$.surname");
+		assertThat(surname).isEqualTo("Garcia");
+
+		String email = documentContext.read("$.email");
+		assertThat(email).isEqualTo("josegarcia@gmail.com");
+
+		int number = documentContext.read("$.number");
+		assertThat(number).isEqualTo(640882919);
+
+		String country = documentContext.read("$.country");
+		assertThat(country).isEqualTo("España");
+	}
+
+	@Test
+	void shouldNotReturnAInfoColWithAnUnknownId() {
+		ResponseEntity<String> response = restTemplate.getForEntity("/infocontrollers/1000", String.class);
+
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+		assertThat(response.getBody()).isNotNull();
+	}
+
 }
